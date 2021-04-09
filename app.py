f61842c66201a563e5bcd90a32f9343d1a9c664b
Sandeep Indana <isandeep.1609@gmail.com>
2021-04-09 09:22:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import logging as logger
+
+logger.basicConfig(level="DEBUG")
+
+
+flaskAppInstance = Flask(__name__)
+
+if __name__ == '__main__':
+    logger.debug("App is running...")
+    from api import *
+    flaskAppInstance.run(host="0.0.0.0", port=5000,
+                         debug=True, use_reloader=True)
